5a87a76f676dd5c96c06f6d15b9213543c15a290
tahsinratul <ratulprofessional123@gmail.com>
2025-08-22 21:38:46 +0600

    last

--- src/app/dashboard/addproducts/page.jsx
+++ src/app/dashboard/addproducts/page.jsx
@@ -36,7 +36,7 @@ export default function AddProduct() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto py-12">
+    <div className="max-w-2xl mx-auto py-12 h-[65vh] px-4">
       <h1 className="text-3xl font-bold mb-6 text-center">Add a Product</h1>
       <form onSubmit={handleSubmit} className="space-y-4 bg-white p-6 rounded-xl shadow-md">
         <input
